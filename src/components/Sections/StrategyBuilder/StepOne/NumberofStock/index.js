d3ee5474b8b37e7b1f1347ac487031d4f2837508
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-07-09 20:49:14 +0400

    updated the values in step 3 to be int not float and updated the range slider as well as the sectors section in step 1

--- src/components/Sections/StrategyBuilder/StepOne/NumberofStock/index.js
+++ src/components/Sections/StrategyBuilder/StepOne/NumberofStock/index.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 import classes from "./index.module.scss";
-// Using an ES6 transpiler like Babel
-import Slider from "react-rangeslider";
-
-// To include the default styles
-import "react-rangeslider/lib/index.css";
-import "./index.scss";
 import { connect } from 'react-redux';
-
-// Not using an ES6 transpiler
 
 class NumberofStock extends Component {
   render() {
@@ -16,31 +8,16 @@ class NumberofStock extends Component {
       <div className={classes.stockNumberContainer}>
         <div className={classes.stockNumberHeading}># Number of Stock</div>
         <div className={classes.rangeSlider}>
-          <div className={classes.stockNumbercontrols}>
-            <div className={`${this.props.value===10 ? classes.active : ''}`}>
-              10
-            </div>
-            <div className={`${this.props.value===20 ? classes.active : ''}`}>
-              20
-            </div>
-            <div className={`${this.props.value===25 ? classes.active : ''}`}>
-              25
-            </div>
-            <div className={`${this.props.value===50 ? classes.active : ''}`}>
-              50
-            </div>
-          </div>
-
-          <Slider
+            <input
+            type="range"
+            name="stock" 
             value={this.props.value}
-            min={1}
-            step={1}
-            max={50}
-            tooltip={false}
-            handleLabel={String(this.props.value)}
-            orientation="horizontal"
+            min="1"
+            max="50"
+            step="1"
             onChange={this.props.stockChange}
-          />
+            className={classes.input}/>
+            <div className={classes.rangelabel}>{this.props.value}</div>
         </div>
       </div>
     );

7618e6eb6f681e7ef2b1a5dae4472dd495c1d784
Noubar Jack Hanno <noubar@Noubars-MacBook-Pro-2.local>
2019-08-02 16:57:32 +0400

    update the actiopn creator of the dashboard as the endpoint payload was converted to a string

--- src/actions/dashboard.js
+++ src/actions/dashboard.js
@@ -1,27 +1,43 @@
-import {
-    DASHBOARD_GET,
-    DASHBOARD_RESET,
-    UPDATE_COUNTRY
-} from './types';
-import wealthface from '../apis/wealthface';
+import { DASHBOARD_GET, DASHBOARD_RESET, UPDATE_COUNTRY } from "./types";
+import wealthface from "../apis/wealthface";
 
-export const getDashboard = (country) => (dispatch, getState) => {
-    wealthface.get('/factor/dashboard', {
-        params: {
-            user_id: getState().auth.userID
-        },
-        headers: {
-          Authorization: `JWT ${getState().auth.token}`
+export const getDashboard = country => (dispatch, getState) => {
+  wealthface
+    .get("/factor/dashboard", {
+      params: {
+        user_id: getState().auth.userID
+      },
+      headers: {
+        Authorization: `JWT ${getState().auth.token}`
+      }
+    })
+    .then(response => {
+      const textContructor = "text".constructor;
+      const objectConstructor = {}.constructor;
+      let responseData;
+      try {
+        if (response.data.constructor === objectConstructor) {
+          responseData = JSON.stringify(response.data);
+        } else if (response.data.constructor === textContructor) {
+          responseData = response.data.replace(/\bNaN\b/g, null);
+          responseData = responseData.replace(/-\bInfinity\b/g, null);
+          responseData = responseData.replace(/\bInfinity\b/g, null);
         }
-    }).then(res => {
-        dispatch({type: DASHBOARD_GET, payload: {data: res.data.message, country:country}});
-    }).catch(err => console.log(err));
-}
+        dispatch({
+          type: DASHBOARD_GET,
+          payload: { data: JSON.parse(responseData).message, country: country }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    })
+    .catch(err => console.log(err));
+};
 
-export const updateCuntry = (country) => dispatch => {
-    dispatch({type: UPDATE_COUNTRY, payload: country});
-}
+export const updateCuntry = country => dispatch => {
+  dispatch({ type: UPDATE_COUNTRY, payload: country });
+};
 
 export const resetDashboard = () => dispatch => {
-    dispatch({type: DASHBOARD_RESET})
-}
+  dispatch({ type: DASHBOARD_RESET });
+};

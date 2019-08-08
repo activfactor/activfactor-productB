3f2b15f8f89c3429d8cdcd7b44ae3e7e81d6258e
Muhammad Ali Manzoor <malimanzoor@getgroup.com>
2019-08-08 13:54:48 +0400

    tables styling fixes for strategy-builder step-two

--- src/components/Sections/StrategyBuilder/StepTwo/Charts/TableChart/index.jsx
+++ src/components/Sections/StrategyBuilder/StepTwo/Charts/TableChart/index.jsx
@@ -6,9 +6,9 @@ class TableChart extends Component {
 
   render() {
     return (
-      <div className="card__table">
+      <div className="card__table _full-height-table">
         <div className="table-responsive">
-          <table className="table table-borderless">
+          <table className="table table-borderless table-hover">
             <thead className={classes.table__header}>
               <tr className={classes.table__headerow}>
                 <th scope="col">{this.props.tableName}</th>
@@ -17,28 +17,26 @@ class TableChart extends Component {
               </tr>
             </thead>
 
-            <tbody className={classes.tablebody}>
+            <tbody>
             {this.props.strategy
               ? Object.keys(this.props.strategy).map((row, i) => {
                 return (
-                  <tr className={classes.table__bodyrow} key={i}>
+                  <tr key={i}>
                     <td
                       data-label={this.props.tableName}
-                      className="_row-name"
+                      className="_td-title"
                     >
-                      <span className={classes.title}>
-                        {CapString(String(row).split("_").join(" "))}
-                      </span>
+                      {CapString(String(row).split("_").join(" "))}
                     </td>
                     <td
                       data-label="Strategy"
-                      className={`_td-strategy ${this.props.strategy[row] > 0 ? '_success' : '_warning'}`}
+                      className={`_td-strategy ${this.props.strategy[row] > 0 ? 'text-primary' : 'text-danger'}`}
                     >
                       {this.props.strategy[row] ? String(this.props.strategy[row].toFixed(2))+ `${["1_year_sharpe_ratio","3_years_sharpe_ratio","5_years_sharpe_ratio","7_years_sharpe_ratio","beta","sharpe_ratio","sortino_ratio","information_ratio"].includes(String(row)) ? '' : '%'}` : "---"}
                     </td>
                     <td
                       data-label={this.props.benchmark}
-                      className={`_td-benchmark ${this.props.benchmark[row] > 0 ? '_success' : '_warning'}`}
+                      className={`_td-benchmark ${this.props.benchmark[row] > 0 ? 'text-primary' : 'text-danger'}`}
                     >
                       {this.props.benchmark[row] ? String(this.props.benchmark[row].toFixed(2))+ `${["1_year_sharpe_ratio","3_years_sharpe_ratio","5_years_sharpe_ratio","7_years_sharpe_ratio","beta","sharpe_ratio","sortino_ratio","information_ratio"].includes(String(row)) ? '' : '%'}` : "---"}
                     </td>

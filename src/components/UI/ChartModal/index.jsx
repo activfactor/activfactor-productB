dcaed8d9240330a09614004d1c0d0d9f154e818b
Noubar Hanno <noubar.hanno@hotmail.com>
2019-09-08 14:38:58 +0400

    updated the blue in all the modals

--- src/components/UI/ChartModal/index.jsx
+++ src/components/UI/ChartModal/index.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import classes from './index.module.scss';
 
-const ChartModal = props => {
-  return ReactDOM.createPortal(
-    <div onClick={props.onDismiss} className="modal__container _chart-modal">
-      <div onClick={e => e.stopPropagation()} className={`message__wrapper ${props.nameOfClass}`}>
-        {props.children}
-      </div>
-    </div>,
-    document.querySelector("#message")
-  );
+class ChartModal extends React.Component {
+  componentDidMount(){
+    document.body.classList.add('modal__active');
+  }
+
+  componentWillUnmount(){
+    document.body.classList.remove('modal__active');
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      <div onClick={this.props.onDismiss} className="modal__container _chart-modal">
+        <div onClick={e => e.stopPropagation()} className={`message__wrapper ${this.props.nameOfClass}`}>
+          {this.props.children}
+        </div>
+      </div>,
+      document.querySelector("#message")
+    );
+  }
 };
 
 export default ChartModal;
--- src/components/UI/Modal/index.jsx
+++ src/components/UI/Modal/index.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import classes from './index.module.scss';
 
-const Message = props => {
-  return ReactDOM.createPortal(
-    <div onClick={props.onDismiss} className="message__container">
-        <div onClick={(e) => e.stopPropagation()} className="message__wrapper">
-            {props.children}
-        </div>
-    </div>,
-    document.querySelector("#message")
-  );
+class Message extends React.Component {
+  componentDidMount(){
+    document.body.classList.add('modal__active');
+  }
+
+  componentWillUnmount(){
+    document.body.classList.remove('modal__active');
+  }
+
+  render(){
+    return ReactDOM.createPortal(
+      <div onClick={this.props.onDismiss} className="message__container">
+          <div onClick={(e) => e.stopPropagation()} className="message__wrapper">
+              {this.props.children}
+          </div>
+      </div>,
+      document.querySelector("#message")
+    );
+  }
 };
 
 export default Message;
